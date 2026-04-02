e945bea84c797e250af14fe891321975bc37f139
Fathurrahman <fathurrahman.work@gmail.com>
2026-04-02 13:59:27 +0700

    chore: remove unused vitest config and commented code [POPE-117]

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,6 @@ import react from '@vitejs/plugin-react';
 
 import { defineConfig } from 'vitest/config';
 
-// import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-
-// import { playwright } from '@vitest/browser-playwright';
-
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
@@ -14,25 +10,5 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
     include: ['tests/**/*.test.{ts,tsx}'],
-    // projects: [
-    //   {
-    //     extends: true,
-    //     plugins: [
-    //       // The plugin will run tests for the stories defined in your Storybook config
-    //       // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-    //       storybookTest({ configDir: path.join(dirname, '.storybook') }),
-    //     ],
-    //     test: {
-    //       name: 'storybook',
-    //       browser: {
-    //         enabled: true,
-    //         headless: true,
-    //         provider: playwright({}),
-    //         instances: [{ browser: 'chromium' }],
-    //       },
-    //       setupFiles: ['.storybook/vitest.setup.ts'],
-    //     },
-    //   },
-    // ],
   },
 });
